9a0906f8a9a7b18f1385c1a5619327f8046e3c61
jesusckantillo <jesuscantillocode@gmail.com>
2023-03-11 09:24:02 -0500

    Added abstract methods for scrapping

--- src/scrapping.py
+++ src/scrapping.py
@@ -12,17 +12,20 @@ url_departaments = "https://guayacan02.uninorte.edu.co/4PL1CACI0N35/registro/res
 url_nrcinfo = "https://guayacan02.uninorte.edu.co/4PL1CACI0N35/registro/resultado_nrc1.php"
 url_courseinfo ="https://guayacan02.uninorte.edu.co/4PL1CACI0N35/registro/resultado_curso1.php"
 
-
-#Open json with dpments info
 dpments_file = "dayta/departamentos.json"
 
 
-def read_dpments(dpments_file:json) ->None:
+
+class webScrapper:
+ 
+ @staticmethod
+ def read_dpments(dpments_file:json) ->None:
     with open(dpments_file) as content:
         result = json.load(content)
     print(result)
-
-def getnrcinfo(code: str)->NRC:
+ 
+ @staticmethod
+ def getnrcinfo(code: str,url_nrcinfo: str)->dict:
     params={
     'valida': 'OK',
     'nrc': code,
@@ -85,10 +88,59 @@ def getnrcinfo(code: str)->NRC:
         'classcode':classcode
     }
 
+ @staticmethod
+ def read_nrcdiv(div:BeautifulSoup)->dict:
+    nrcdiv = div
+    #Modeling data from an nrc scraping:
+    all_p = nrcdiv.find_all('p')
+
+    #NRCNAME
+    name = all_p[0].get_text()
 
 
+    #NRCCODE
+    third_p = all_p[2]
+    nrc = third_p.get_text(separator="|").split("|")[5].strip()
 
-def get_allnrc_dpt(dpment_code:str)->list[list[str]]:
+
+    #QUOTAS
+    five_p = all_p[4]
+    quotas = five_p.get_text(separator="|").split("|")[3]
+
+
+    #CLASSCODE
+    second_p = all_p[2]
+    classcode = second_p.get_text(separator="|").split("|")[1].strip()
+
+
+    #BLOCKS
+    teachers=[]
+    #--Extract information from all table rows except for the first one
+    blocks =[]
+    table = nrcdiv.find('table')
+    table_colums = table.find_all('tr')
+    table_colums = table_colums [1:]
+    for colum in table_colums:
+        all_td = colum.find_all('td')
+        block = [td.get_text(strip=True) for td in all_td]
+        block = block[2:]
+        teachers.append(block[3])
+        blocks.append(block)
+
+
+    #TEACHERS
+    teachers = list(set(teachers))
+    return {
+        'teacher':teachers,
+        'name':name,
+        'blocks':blocks,
+        'quotas':quotas,
+        'nrc':nrc,
+        'classcode':classcode
+    }
+
+ @staticmethod
+ def get_allnrc_dpt(dpment_code:str,url_departaments)->json:
     all_nrc =[]
     params = {
     'departamento': dpment_code,
@@ -105,34 +157,35 @@ def get_allnrc_dpt(dpment_code:str)->list[list[str]]:
       all_nrc.append(nrc)
     return all_nrc
 
-def get_allcourses_dpt(dpment_code:str, dpment_name:str,):
+ @staticmethod
+ def get_allcourses_dpt(dpment_code:str,getallnrcfunc: callable):
     unique = {}
-    all_nrc = get_allnrc_dpt(dpment_code)
+    all_nrc = getallnrcfunc(dpment_code)
     for nrc in all_nrc:
         class_name = nrc[1]
         class_code = nrc[2]
         if class_name not in unique:
             unique[class_name] = class_code
-    with open(dpment_name, "w") as outfile:
-      json.dump(get_allcourses_dpt("0029"), outfile,indent=4)
 
-
-def get_allnrcbycode(classcode:str):
+ @staticmethod
+ def get_allnrcbycode(classcode:str):
     params = {
         'valida': 'OK',
         'mat2': classcode[:3],
         'curso': classcode[3:],
         'BtnCurso': 'Buscar',
 
-        'datos_periodo': 20310,
+        'datos_periodo': 202310,
         'nom_periodo': 'Primer Semestre 2023',
         'datos_nivel': 'PR',
         'nom_nivel' : 'Pregrado'
     }
-    response = requests.post(url_courseinfo,params)
+    response = requests.post(url_courseinfo,data=params)
     soup = BeautifulSoup(response.text,"lxml")
-    print(classcode[:3])
-    print(classcode[3:])
-    print(soup.prettify)    
+    all_divs = soup.find_all('div',class_='div')
+    for div in all_divs:
+        print(read_nrcdiv(div))
 
-print(getnrcinfo("2894"))
+print(webScrapper.getnrcinfo("2894",url_nrcinfo))
+
+
