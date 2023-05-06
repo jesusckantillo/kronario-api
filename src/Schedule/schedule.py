673fd7e6afb0598ec95b4fbf32b952713a87448d
Jesus David Cantillo Guerrero <jesuscantillocode@gmail.com>
2023-05-06 12:48:44 -0500

    New methods

--- src/Schedule/schedule.py
+++ src/Schedule/schedule.py
@@ -22,26 +22,31 @@ class scheduleController():
          #for combination in combinations:
              #if check_combination(combination):
                 # valid_combinations.append(combination)
-             
+
 
     @staticmethod
+    def parse_blocks(blocks):
+        result = []
+        for block in blocks:
+         day, time, room = block
+         start_str, end_str = time.split(' - ')
+         start = datetime.strptime(start_str, '%H%M')
+         end = datetime.strptime(end_str, '%H%M')
+         result.append((start, end, day))
+        return result
 
-    
 
-    @staticmethod
-    def 
-    
-    @staticmethod
-    def nrc_has_conflict(olds_nrcs: "NRC", nrc:"NRC")->bool:
+    #@staticmethod
+    #def nrc_has_conflict(olds_nrcs: "NRC", nrc:"NRC")->bool:
 
 
- 
-    @staticmethod
-    def check_combination(combination: List[NRC]):
-        valid_nrcs = [combination[0]]
-        for nrc in combination:
-            if scheduleController.nrc_has_conflict(valid_nrcs, nrc):
+        
+   # @staticmethod
+    #def check_combination(combination: List[NRC]):
+       # valid_nrcs = [combination[0]]
+       # for i in range(len(combination)):
+         #   if scheduleController.nrc_has_conflict(valid_nrcs, nrc):
 
-        return True
+       # return True
 
-scheduleController.parser_block(['F', '0930 - 1128', 'BLOQG2 - 34G2'])
+scheduleController.parse_blocks([['F', '0930 - 1128', 'BLOQG2 - 34G2']])
