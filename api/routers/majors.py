b57f90051426f39f005517abfb623e4f473e50d9
jesusckantillo <jesuscantillocode@gmail.com>
2023-05-28 16:28:42 -0500

    New models for majors schema with id and category implemented in the endpoint

--- api/routers/majors.py
+++ api/routers/majors.py
@@ -9,15 +9,14 @@ majors = APIRouter()
 @majors.get("/majors", response_model=List[Majors])
 async def get_majors():
     db_majors = crud.get_majors()
-
     return [Majors(name=major.name, id=major.major_code, category="Pregrado" if major.major_code[2] == "E" else "Posgrado") for major in db_majors]
 
      
 
 @majors.post("/majors/classcodes")
 async def get_majors_classcodes(major_data: List[Majors]):
-    classcodes = list(itertools.chain.from_iterable([crud.get_majors_classcodes(major.major_code) for major in major_data]))
-    majors = [classcode.majors for classcode in classcodes]
-    majors_name = [[major.name for major in major_list] for major_list in majors]
-    cc = [Classcodes(name=classcode.name, cc_code=classcode.cc_code,majorsinf=[major.name for major in classcode.majors])for classcode in classcodes]
+    classcodes = list(itertools.chain.from_iterable([crud.get_majors_classcodes(major.id) for major in major_data]))
+    majors_sent = [major.name for major in major_data]
+    print(majors)
+    cc = [Classcodes(name=classcode.name, cc_code=classcode.cc_code,majorsinf=[major.name for major in classcode.majors if major.name in majors_sent])for classcode in classcodes]
     return cc
