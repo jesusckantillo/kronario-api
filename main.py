2ebc01e4aeadea060dd26ce9073e21fb97459f11
jesusckantillo <jesuscantillocode@gmail.com>
2023-05-28 16:32:00 -0500

    Cors allowed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from api.routers.majors import majors
 from api.routers.teachers import teachers
 from api.routers.scheduler import schedule_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(majors)
@@ -11,3 +13,13 @@ app.include_router(schedule_router)
 @app.get(path="/")
 def home():
     return {"Kronario API: Working"}
+
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
