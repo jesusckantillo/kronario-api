eabeccfe34c744dc200346f1f6d9c2df360726e2
jesusckantillo <jesuscantillocode@gmail.com>
2023-05-29 02:45:50 -0500

    Scheduler endpoint featrue for new json information request

--- api/routers/scheduler.py
+++ api/routers/scheduler.py
@@ -6,11 +6,48 @@ import schemas.schemas
 
 schedule_router = APIRouter()
 
+""""
+"subjects": [
+        {
+            "name": "CIRCUITOS I ",
+            "id": "IEL1011",
+            "category": [
+                "Ingeniería Eléctrica"
+            ]
+        },
+        {
+            "name": "ELECTRONICA II ",
+            "id": "IEN4030",
+            "category": [
+                "Ingeniería Eléctrica"
+            ]
+        },
+        {
+            "name": "CALCULO II ",
+            "id": "MAT1111",
+            "category": [
+                "Ingeniería de Sistemas y Computación",
+                "Ingeniería Eléctrica"
+            ]
+        }
+        """""
+
 
 @schedule_router.post("/schedule")
-async def create_schedule(list_classcodes: List[str], time: List[schemas.schemas.TimeFilter] = None, professor: schemas.schemas.ProfessorFilter = None):
+async def create_schedule(info:dict):
+    subjects = info["subjects"]
+    list_classcodes = [subject["id"] for subject in subjects]
+    if len(info["time"]) ==0:
+        time = None
+    else:
+        time = [schemas.schemas.TimeFilter(**time) for time in info["time"]]
+    if len(info["professors"]) ==0:
+        professor = None
+    else:
+        professor = schemas.schemas.ProfessorFilter(professors=[professor["name"] for professor in info["professors"]])
     schedules = []
     info = scheduleController.create_schedule(list_classcodes,time,professor)
     for schedule in info:
         schedules.append([nrc.to_dict() for nrc in schedule])
+    
     return schedules
